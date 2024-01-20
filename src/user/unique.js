4a49555972cb3d45ec81a9a500e12e892d21c711
latestbob <edidiongbobson@gmail.com>
2024-01-19 16:53:15 -0800

    almost done governor

--- src/user/unique.js
+++ src/user/unique.js
@@ -18,8 +18,11 @@ function UniqueDashboard(){
    
     // const[summary, setSummary] = useState("");
 
+    const[summary, setSummary] = useState("");
+
     const location = useLocation();
     const[mdaname, setMdaName] = useState(location.state.mdaname);
+    const[mdasInitiatives , setMdasInitiatives] = useState([]);
 
     React.useEffect(()=>{
         var docId = localStorage.getItem("authid");
@@ -34,6 +37,46 @@ function UniqueDashboard(){
 
      
     },[]);
+
+
+    useEffect(() => {
+        console.log('working');
+
+        if(mdaname != ""){
+        
+               axios.get(`https://office.laurenparkerway.com/api/mda/summary?mda=${mdaname}`)
+                .then(response => {
+                 
+                    setSummary(response.data);
+                  console.log('fetched');
+                  console.log(response.data);
+                    console.log(response.data);
+                })
+                .catch(error => {
+                  console.log(error);
+                });
+            }
+      }, [mdaname]);
+
+
+      useEffect(() => {
+        console.log('working');
+
+        if(mdaname != ""){
+        
+               axios.get(`https://office.laurenparkerway.com/api/mdas/unique?mda=${mdaname}`)
+                .then(response => {
+                 
+                    setMdasInitiatives(response.data);
+                  console.log('fetched');
+                  console.log(response.data);
+                    console.log(response.data.length);
+                })
+                .catch(error => {
+                  console.log(error);
+                });
+            }
+      }, [mdaname]);
 
 
     
@@ -150,6 +193,83 @@ function UniqueDashboard(){
 
                 {/* activity count */}
 
+
+
+                <div className='row'>
+                        <div className='col-md-6 mb-3'>
+                            <div className='count bg-danger rounded px-3'>
+
+                                <div>
+                                    <p className='title'>Late <br/> <span className='figure'>{summary.late_percent}%</span></p>
+
+                                   
+                                </div>
+
+                                <i className='fa fa-exclamation-triangle icontwo text-light'></i>
+                                
+                            </div>
+                        </div>
+
+                        <div className='col-md-6 mb-3'>
+                            <div className='count bg-secondary rounded px-3'>
+                            <div>
+                                    <p className='title'>Not Started <br/> <span className='figure'>{summary.notstarted_percent}%</span></p>
+
+                                   
+                                </div>
+
+                                <i className='fa fa-clock icontwo text-light'></i>
+                                
+                              
+                            </div>
+                        </div>
+
+                        
+
+                       
+                    </div>
+
+
+
+
+
+
+
+
+<div className='row'>
+                        <div className='col-md-6 mb-3'>
+                            <div className='count bg-warning rounded px-3'>
+
+                                <div>
+                                    <p className='titledark'>On Time <br/> <span className='figuredark'>{summary.ontime_percent}%</span></p>
+
+                                   
+                                </div>
+
+                                <i className='fa fa-gear icontwo text-dark'></i>
+                                
+                            </div>
+                        </div>
+
+                        <div className='col-md-6 mb-3'>
+                            <div className='count bg-success rounded px-3'>
+                            <div>
+                                    <p className='title'>Completed <br/> <span className='figure'>{summary.complete_percent}%</span></p>
+
+                                   
+                                </div>
+
+                                <i className='fa fa-check icontwo text-light'></i>
+                                
+                              
+                            </div>
+                        </div>
+
+                        
+
+                    </div>
+
+
                 
 
 
@@ -157,7 +277,92 @@ function UniqueDashboard(){
                 
 
                 
+                    <div className='table-responsive tablediv'>
 
+                        <table className='table table-striped table-hover table-borderless'>
+
+                            <thead className=''>
+                                <tr>
+                                    
+                                    <th>Initiative</th>
+                                    
+                                    <th>Estimated Completion Date</th>
+                                    <th>Estimated Budget (NGN)</th>
+                                  
+                                    
+                                    <th>Stage</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+
+                            </thead>
+
+
+                            <tbody>
+                            
+
+
+                        {mdasInitiatives && mdasInitiatives.map((m, index) => (
+                            // Your rendering logic for each item goes here
+                            <tr key={index}>
+                                        
+                                <td>{m.initiative}</td>
+                               
+                                <td>{m.date}</td>
+                                <td>{m.budget}</td>
+                                
+                              
+                                <td>
+                                <span>{m.stage}</span>
+                                </td>
+
+                                <td>
+                                <span>{m.status}</span>
+                                </td>
+                                <td>
+                                   
+                                </td>
+
+                                <div class="modal fade" id={`exampleModalLong${index}`} tabindex="-1" role="dialog" aria-labelledby={`exampleModalLongTitle${index}`} aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Edit {m.initiative}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+                   
+      </div>
+     
+    </div>
+  </div>
+</div>
+
+
+           
+                                      
+                                         </tr> 
+
+                                // Edit modal
+
+                                
+
+
+
+                                //end of edit modal
+                        ))
+
+                        }
+
+                                    
+                        </tbody>
+
+
+                        </table>
+
+                    </div>
 
 
                    
